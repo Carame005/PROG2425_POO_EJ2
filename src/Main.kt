3abb84c56fb202c3de982a522a7c0050f6069b74
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-21 19:13:45 +0100

    inicial

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!dfssfaefe")
+class Persona(var peso : Double, var altura : Double){
+    var nombre : String = ""
+    var imc : Double = peso*(altura*altura)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    constructor(peso: Double,altura: Double, nombrenuevo : String) : this(peso,altura){
+        this.nombre = nombrenuevo
     }
+
+    override fun toString(): String {
+        return "$nombre, de altura $altura metros,peso de $peso kilos, tiene un IMC de $imc"
+    }
+}
+
+fun main(){
+    val persona1 = Persona(69.6,1.8)
+    persona1.nombre = readln()
+    println(persona1.nombre)
+    println(persona1.peso)
+    println(persona1.altura)
+
+    val persona2 = Persona(68.0,1.6,"Eustaquio")
+    println(persona2)
+    persona2.altura = 1.8
+    println(persona2.peso)
+    println(persona2.altura)
+    println(persona2.imc)
+
+    val persona3 = Persona(70.5,1.2)
+    persona3.altura = 1.8
+    println(persona2)
+    println(persona3)
+    println(persona2.equals(persona3))
+
 }
